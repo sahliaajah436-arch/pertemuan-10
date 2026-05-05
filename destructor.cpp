4dd443160e0dd25629971ec60b16b48ebc0690d0
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-05 19:38:01 +0700

    menambahkan prosedur dan menambahkan fungsi int main

--- destructor.cpp
+++ destructor.cpp
@@ -26,3 +26,31 @@ angka::~angka(){ // Destructor
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i];
+    }
+    cout << endl;
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);              // Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
+    delete ptrBelajarcpp;             // Destructor Dipanggil
+
+    return 0;
+}
