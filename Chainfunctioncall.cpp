4d1851d333847cb6661f978d5f5cd922215bf3f0
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-05 19:41:16 +0700

    membuat class buku

--- Chainfunctioncall.cpp
+++ Chainfunctioncall.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    // Fungsi untuk set judul yang mengembalikan referensi ke objek itu sendiri (*this)
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this; // Inilah kunci dari chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
