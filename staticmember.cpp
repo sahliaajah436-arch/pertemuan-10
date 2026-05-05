fade64bc0709b444a6cc31b6c60da9c44aabb346
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-05 19:31:44 +0700

    manambahkan prosedur dan memanggil prosedur printAll()

--- staticmember.cpp
+++ staticmember.cpp
@@ -19,3 +19,32 @@ class Mahasiswa {   //membuat class mahasiswa
             setID();
         }
 };
+
+int Mahasiswa::nim = 0;
+
+//pendefinisian prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+    cout << "ID : " << id << endl;
+    cout <<"Nama : " << nama << endl;
+    cout << endl;
+};
+
+int main(){
+    //membuat object dan membeeri nilai
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
+}
